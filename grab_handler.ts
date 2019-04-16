5c4252d93d141bea60bf96d51bf2670a33f49992
Jaslyn <jaslyn@siliconjungles.com>
2019-04-16 15:49:01 +0800

    Added second handler

---
+++ grab_handler.ts
@@ -0,0 +1,13 @@
+import { APIGatewayProxyResult, Callback, Context, Handler } from 'aws-lambda'
+
+const checkPrice: Handler = (event: any, context: Context, callback: Callback) => {
+	const response: APIGatewayProxyResult = {
+		statusCode: 200,
+		body: JSON.stringify({
+				grab_price: '123'
+		})
+	}
+	callback(undefined, response)
+}
+
+export { checkPrice }
--- handler.ts
+++ handler.ts
@@ -1,29 +1,22 @@
-import { Callback, Context, Handler } from 'aws-lambda'
-
-interface HelloResponse {
-	statusCode: number
-	body: string
-}
+import { APIGatewayProxyResult, Callback, Context, Handler } from 'aws-lambda'
 
 const main: Handler = (event: any, context: Context, callback: Callback) => {
-	const response: HelloResponse = {
+	const response: APIGatewayProxyResult = {
 		statusCode: 200,
 		body: JSON.stringify({
-				message: Math.floor(Math.random() * 1000)
+			message: Math.floor(Math.random() * 1000)
 		})
 	}
-
 	callback(undefined, response)
 }
 
 const secondary: Handler = (event: any, context: Context, callback: Callback) => {
-	const response: HelloResponse = {
-			statusCode: 200,
-			body: JSON.stringify({
-					message: 'secondary'
-			})
+	const response: APIGatewayProxyResult = {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: 'secondary'
+		})
 	}
-
 	callback(undefined, response)
 }
 

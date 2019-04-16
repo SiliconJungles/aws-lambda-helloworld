dda37880520eed5d7322b6e515c7b0ff304d0963
Jaslyn <jaslyn@siliconjungles.com>
2019-04-16 14:12:30 +0800

    Commit project files

---
+++ handler.ts
@@ -0,0 +1,19 @@
+import { Callback, Context, Handler } from 'aws-lambda'
+
+interface HelloResponse {
+	statusCode: number
+	body: string
+}
+
+const main: Handler = (event: any, context: Context, callback: Callback) => {
+	const response: HelloResponse = {
+		statusCode: 200,
+		body: JSON.stringify({
+				message: Math.floor(Math.random() * 1000)
+		})
+	}
+
+	callback(undefined, response)
+}
+
+export { main }

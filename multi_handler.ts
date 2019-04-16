863914e4547a2d2946a983259d0dfad75992c814
Jaslyn <jaslyn@siliconjungles.com>
2019-04-16 16:14:04 +0800

    Query parameters

--- multi_handler.ts
+++ multi_handler.ts
@@ -50,4 +50,30 @@ const function5: Handler = (event: any, context: Context, callback: Callback) =>
 	callback(undefined, response)
 }
 
-export { function1, function2, function3, function4, function5 }
+const getParams: Handler = (event: any, context: Context, callback: Callback) => {
+	const params = event.queryStringParameters
+	const response: APIGatewayProxyResult = {
+		statusCode: 200,
+		body: JSON.stringify({
+			method: 'get',
+			a: params.a,
+			b: params.b
+		})
+	}
+	callback(undefined, response)
+}
+
+const postParams: Handler = (event: any, context: Context, callback: Callback) => {
+	const params = event.queryStringParameters
+	const response: APIGatewayProxyResult = {
+		statusCode: 200,
+		body: JSON.stringify({
+			method: 'post',
+			a: params.a,
+			b: params.b
+		})
+	}
+	callback(undefined, response)
+}
+
+export { function1, function2, function3, function4, function5, getParams, postParams }

1ee74323fb32117ef36e8aa71d8582fdc092f1b5
Jaslyn <jaslyn@siliconjungles.com>
2019-04-18 13:55:52 +0800

    Return promise instead of using callbacks

--- grab_handler.ts
+++ grab_handler.ts
@@ -1,14 +1,14 @@
 import { APIGatewayProxyResult, Callback, Context, Handler } from 'aws-lambda'
 import { all_prices } from './src/api'
 
-const checkPrice: Handler = async (event: any, context: Context, callback: Callback) => {
+const checkPrice: Handler = async (event: any, context: Context) => {
 	const response: APIGatewayProxyResult = {
 		statusCode: 200,
 		body: JSON.stringify(
 				await all_prices()
 		)
 	}
-	callback(undefined, response)
+	return response
 }
 
 export { checkPrice }

ab194e0072c155d2ba49e1c124a7fc9156313003
Jaslyn <jaslyn@siliconjungles.com>
2019-04-18 14:12:49 +0800

    Attempt to fix async

--- grab_handler.ts
+++ grab_handler.ts
@@ -1,8 +1,19 @@
 import { APIGatewayProxyResult, Callback, Context, Handler } from 'aws-lambda'
 import * as API from './src/api'
 
-const checkPrice: Handler = async (event: any, context: Context) => {
+// const checkPrice: Handler = async (event: any, context: Context) => {
 
+// 	const prices = JSON.stringify(await API.all_prices())
+// 	const response: APIGatewayProxyResult = {
+// 		statusCode: 200,
+// 		body: prices
+// 	}
+// 	return response
+// }
+
+// export { checkPrice }
+
+module.exports.checkPrice = async (event: any, context: Context) => {
 	const prices = JSON.stringify(await API.all_prices())
 	const response: APIGatewayProxyResult = {
 		statusCode: 200,
@@ -10,5 +21,3 @@ const checkPrice: Handler = async (event: any, context: Context) => {
 	}
 	return response
 }
-
-export { checkPrice }

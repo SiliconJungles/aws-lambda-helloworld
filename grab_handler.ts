cbbf4c5ef625a86af8d12dee72167014f3a6fbd9
Jaslyn <jaslyn@siliconjungles.com>
2019-04-18 14:00:27 +0800

    Broken test async

--- grab_handler.ts
+++ grab_handler.ts
@@ -1,12 +1,12 @@
 import { APIGatewayProxyResult, Callback, Context, Handler } from 'aws-lambda'
-import { all_prices } from './src/api'
+import * as API from './src/api'
 
 const checkPrice: Handler = async (event: any, context: Context) => {
+
+	const prices = JSON.stringify(await API.all_prices())
 	const response: APIGatewayProxyResult = {
 		statusCode: 200,
-		body: JSON.stringify(
-				await all_prices()
-		)
+		body: prices
 	}
 	return response
 }

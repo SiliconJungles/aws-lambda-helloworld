af8d6ab687447a77068f0649f249018111c28c0d
Jaslyn <jaslyn@siliconjungles.com>
2019-04-18 17:15:39 +0800

    Only package required dependencies

--- grab_handler.js
+++
@@ -1,9 +0,0 @@
-import * as API from './src/api'
-
-exports.handler = async (event) => {
-	const response = {
-			statusCode: 200,
-			body: JSON.stringify(await API.all_prices()),
-	}
-	return response
-}
